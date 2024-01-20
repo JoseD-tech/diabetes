a8671bc041e563099910be3312eb2aa08c474b03
JoseD-tech <jgmting@gmail.com>
2024-01-20 19:30:53 -0400

    agregando cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,45 +24,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Estado::create([
-            'estado' => 'Pendiente'
-        ]);
 
-        Estado::create([
-            'estado' => 'Revisado'
-        ]);
-
-        //Crea los Roles
-        $role1 = Role::create(['name' => 'admin']);
-        $role2 = Role::create(['name' => 'doctor']);
-        $role3 = Role::create(['name' => 'secretario']);
-
-        // Usuarios de Prueba admin/usuario
-        User::create([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin123'),
-        ]);
-
-        User::create([
-            'name' => 'doctor',
-            'email' => 'doctor@gmail.com',
-            'password' => Hash::make('doctor123'),
-        ]);
-
-        User::create([
-            'name' => 'secretario',
-            'email' => 'secretario@gmail.com',
-            'password' => Hash::make('secretario123'),
-        ]);
-
-        $user = User::find(1);
-        $user->assignRole($role1);
-
-        $user2 = User::find(2);
-        $user2->assignRole($role2);
-
-        $user3 = User::find(3);
-        $user3->assignRole($role3);
     }
 }
