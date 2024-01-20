a22e99ca8c5677cc4986a982cf33d32ef866c2dc
JoseD-tech <jgmting@gmail.com>
2024-01-20 19:00:07 -0400

    agregando cambios

--- database/migrations/2024_01_20_130526_create_estados_table.php
+++ database/migrations/2024_01_20_130526_create_estados_table.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Role;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -20,39 +16,6 @@ return new class extends Migration
             $table->string('estado');
             $table->timestamps();
         });
-
-        //Crea los Roles
-        $role1 = Role::create(['name' => 'admin']);
-        $role2 = Role::create(['name' => 'doctor']);
-        $role3 = Role::create(['name' => 'secretario']);
-
-        // Usuarios de Prueba admin/usuario
-        User::create([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin123'),
-        ]);
-
-        User::create([
-            'name' => 'doctor',
-            'email' => 'doctor@gmail.com',
-            'password' => Hash::make('doctor123'),
-        ]);
-
-        User::create([
-            'name' => 'secretario',
-            'email' => 'secretario@gmail.com',
-            'password' => Hash::make('secretario123'),
-        ]);
-
-        $user = User::find(1);
-        $user->assignRole($role1);
-
-        $user2 = User::find(2);
-        $user2->assignRole($role2);
-
-        $user3 = User::find(3);
-        $user3->assignRole($role3);
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Estado;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +23,46 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Estado::create([
+            'estado' => 'Pendiente'
+        ]);
+
+        Estado::create([
+            'estado' => 'Revisado'
+        ]);
+
+        //Crea los Roles
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'doctor']);
+        $role3 = Role::create(['name' => 'secretario']);
+
+        // Usuarios de Prueba admin/usuario
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+        ]);
+
+        User::create([
+            'name' => 'doctor',
+            'email' => 'doctor@gmail.com',
+            'password' => Hash::make('doctor123'),
+        ]);
+
+        User::create([
+            'name' => 'secretario',
+            'email' => 'secretario@gmail.com',
+            'password' => Hash::make('secretario123'),
+        ]);
+
+        $user = User::find(1);
+        $user->assignRole($role1);
+
+        $user2 = User::find(2);
+        $user2->assignRole($role2);
+
+        $user3 = User::find(3);
+        $user3->assignRole($role3);
     }
 }
