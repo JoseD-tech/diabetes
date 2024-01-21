f89a928e8aba00ba89ff620e8aaafd556437aa33
JoseD-tech <jgmting@gmail.com>
2024-01-20 21:14:43 -0400

    agregando cambios

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use App\Http\Controllers\SecretariaController;
 // composer install --ignore-platform-reqs && npm install && npm run build && php artisan optimize && php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan migrate --force
 
 Route::get('/', function () {
-    return Inertia::render('Auth/Login');
+    return to_route('login');
 });
 
 Route::get('/dashboard', function () {
