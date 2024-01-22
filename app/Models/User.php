dac6c5a9833f687ec697c8bd297f6f899bd8ed56
JoseD-tech <jgmting@gmail.com>
2024-01-21 22:49:19 -0400

    cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,8 +45,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class, 'model_has_roles', 'model_id', 'role_id');
-    }
+
 }
